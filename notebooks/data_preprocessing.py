340955b57bbe08a465c3427f7c137470f279713f
harbenml <harbenml@pm.me>
2020-07-10 13:54:09 +0200

    add new features for modeling

--- notebooks/data_preprocessing.py
+++ notebooks/data_preprocessing.py
@@ -17,10 +17,12 @@
 
 # ## TODO
 #
+# - data cleaning
+#     - impute NaN in fare column of test set
 # - feature engineering
-#      - family size = SibSp + Parch + 1
-#      - Title
-#      - Age = f(other features)
+#     - family size = SibSp + Parch + 1
+#     - Title
+#     - Age = f(other features)
 
 # ## Imports and helper functions
 
@@ -37,6 +39,7 @@ from sklearn.svm import SVC
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import cross_val_score, StratifiedKFold
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import RandomForestRegressor
 
 
 # -
@@ -70,11 +73,17 @@ train.dtypes
 
 display_all(train)
 
-# Display the amount of NaN data per column
+# Display the amount of NaN data per column in train set
 
 display_all( train.isnull().sum().sort_index()/len(train) )
 
+# Display the amount of NaN data per column in test set
+
+display_all( test.isnull().sum().sort_index()/len(test) )
+
 train.describe()
+
+sns.pairplot(train)
 
 # ## Data cleaning and feature engineering
 
@@ -88,72 +97,132 @@ data.reset_index(inplace=True)
 
 data.dtypes
 
-# Convert categorical columns and do label encoding and inspect the data types again
+
+# ### Get surname and title from name column
+
+def extract_features_from_names(names):
+    surnames = [name.split(',')[0] for name in names]
+    remainder = [name.split(',')[1].strip() for name in names]
+    titles = [x.split('.')[0] for x in remainder]
+    return surnames, titles
+
+
+surnames, titles = extract_features_from_names(data['Name'])
+
+# Unique titles in names:
+
+print(set(titles))
+
+# Replace *Name* columns with the newly generated features
+
+data['Title'] = titles
+data['Surname'] = surnames
+
+data.drop('Name', axis=1, inplace=True)
+
+# ### Create new feature Family Size
+
+data['Family_Size'] = data['SibSp'] + data['Parch'] + 1
+
+# ### Convert categorical columns (label encoding)
 
 # +
 # label encoding
-cols_to_encode = ['Sex', 'Cabin', 'Ticket', 'Embarked']
+cols_to_encode = ['Sex', 'Cabin', 'Ticket', 'Embarked', 'Title', 'Surname']
 for col in cols_to_encode:
-    data[col] = data[col].astype('category').cat.codes
+    data[col] = data[col].astype('category').cat.codes + 1
     
 data.dtypes
 # -
 
-data.drop('Name', axis=1)
+# ### Impute the missing Fare value
 
-# Replace NaNs with the median value
+data[data['Fare'].isna()]
 
-median_age = data["Age"].median()
-data["Age"].fillna(median_age, inplace=True)
+# Let's replace the missing value with the median Fare value of Pclass 3.
 
-median_fare = data["Fare"].median()
-data["Fare"].fillna(median_fare, inplace=True)
+median_fare_pclass3 = data[data['Pclass']==3]['Fare'].median()
+data["Fare"].fillna(median_fare_pclass3, inplace=True)
+data.loc[1043]
+
+
+
+# ### Make a regression model for age to impute missing values
+
+age_model_input = data[['Pclass', 'Sex', 'SibSp', 'Parch', 'Fare', 'Embarked', 'Title', 'Family_Size']]
+
+idx_age_isna = data['Age'].isnull()
+sum(idx_age_isna)
+
+age_model_train_input, age_targets = age_model_input[~idx_age_isna], data['Age'][~idx_age_isna]
+age_model_inference_input = age_model_input[idx_age_isna]
+
+len(age_model_train_input), len(age_targets)
+
+len(age_model_inference_input)
+
+len(data)-1046-263
+
+SEED = 42
+age_mdl = RandomForestRegressor(n_jobs=-1,
+                                n_estimators=1000,
+                                max_features='sqrt',
+                                min_samples_leaf=10,
+                                max_depth=5,
+                                oob_score=True,
+                                random_state=SEED)
+
+age_mdl.fit(age_model_train_input, age_targets)
+
+age_mdl.oob_score_
+
+ages_to_impute = age_mdl.predict(age_model_inference_input)
+
+data.loc[idx_age_isna, 'Age'] = ages_to_impute
+
+display_all( data )
+
+# ### Check, if everything is cleaned now
 
 display_all( data.isna().sum().sort_index()/len(data) )
 
-# ### Create training and test data
+# ## Create training and test data
 
-features = ["Pclass", "Sex", "Age", "SibSp", "Parch", "Fare"]
+data.columns
+
+# +
+features = ["Pclass", "Sex", "Age", "SibSp", "Parch", "Fare", 'Embarked', 'Title']
+
 X = data.loc[:len(train)-1, features]
 X_test = data.loc[len(train):, features]
 print(y.shape)
 print(X.shape)
 print(X_test.shape)
-
-train.tail()
+# -
 
 X.tail()
 
-# + [markdown] heading_collapsed=true
 # ## Correlation analysis
 
-# + hidden=true
-corr = train.corr()
+corr = X.corr()
 print(corr)
 
-# + hidden=true
 f, ax = plt.subplots(figsize=(10,8))
 sns.heatmap(corr, mask=np.zeros_like(corr, dtype=np.bool), cmap=sns.diverging_palette(220, 10, as_cmap=True),
             square=True, ax=ax)
-# -
 
 # # Modeling
 
-# +
-
 print(X.isna().any())
-# -
 
-
-
-SEED = 42
+SEED = 123
 MODELS = {'SVC': make_pipeline(StandardScaler(), SVC(gamma="auto", random_state=SEED)),
           'RandomForest': RandomForestClassifier(n_jobs=-1,
-                                                 n_estimators=1000,
+                                                 n_estimators=2000,
                                                  max_features='sqrt',
                                                  min_samples_leaf=4,
                                                  oob_score=True,
-                                                 random_state=RANDOM_STATE)}
+                                                 random_state=SEED)}
 
 score_means = []
 for MODEL in MODELS:
@@ -176,41 +245,5 @@ for MODEL in MODELS:
     predictions.to_csv(f"model_{MODEL}.csv", index=False)
 
 
-
-
-
-model_1 = pd.read_csv("model_RandomForest.csv")
-
-model_1.head()
-
-model_1.loc[6:10,:]
-
-
-
-model_2 = pd.read_csv("model_SVC.csv")
-
-model_2.head()
-
-blend = round(0.49*model_1.Survived + 0.51*model_2.Survived)
-
-blend.head()
-
-sum(blend-model_2.Survived)
-
-
-
-test["Survived"] = clf.predict(x_test)
-submission = test[["PassengerId", "Survived"]]
-submission.to_csv("submission.csv", index=False)
-
-test
-
-len(clf.predict(x_test))
-
-# +
-string = '  abc '
-
-string.strip()
-# -
 
 
